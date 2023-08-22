f3033ab4cc6fbc485e6e11e383162ebb57ed79d6
haikaljs <haikaljavascript@gmail.com>
2023-08-22 08:27:21 +0800

    Toggle user menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
@@ -15,6 +17,12 @@ const Navbar = () => {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Haikal Rozhan",
+    isSeller: true,
+  };
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -31,8 +39,32 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+
+          {/* toggle user menu */}
+          {!currentUser?.isSeller && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <div>Orders</div>
+                  <div>Messages</div>
+                  <div>Logout</div>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
       {active && (
