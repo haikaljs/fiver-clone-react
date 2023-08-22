e5cfd3bf58922234283538d55d5291226018aed8
haikaljs <haikaljavascript@gmail.com>
2023-08-22 13:22:08 +0800

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
 import "./index.scss";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 };
