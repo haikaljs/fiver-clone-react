c28518c07653c62a8bff9cd9b524607b5ff00ff0
haikaljs <haikaljavascript@gmail.com>
2023-08-23 12:35:57 +0800

    Finishing myGigs page

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -22,7 +22,7 @@ function Gigs() {
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">Fiverr > Graphics & Design ></span>
+        <span className="breadcrumbs">Fiverr {'>'}  Graphics & Design {'>'} </span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Liverr's AI artists
